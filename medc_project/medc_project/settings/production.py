875ecbcbb6ffafab0c31c646dbf632d38268b132
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2015-09-07 00:04:34 -0600

    Minor changes for the production.py in settings

--- medc_project/medc_project/settings/production.py
+++ medc_project/medc_project/settings/production.py
@@ -17,10 +17,10 @@ DATABASES['default']['ENGINE'] = 'django_postgrespool'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
