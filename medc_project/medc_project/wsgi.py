67211e598f46f89a0094e0ac58dc12184390a1ba
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2015-09-06 23:56:38 -0600

    Initial set up to go to production server among them are : new organization for settings, installation requiered by heroku added .gitignore

--- medc_project/medc_project/wsgi.py
+++ medc_project/medc_project/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medc_project.settings")
 
 application = get_wsgi_application()
+
+# Heroku live settings
+
+try:
+    from dj_static import Cling
+
+    aplication = Cling(get_wsgi_application())
+except:
+    pass
